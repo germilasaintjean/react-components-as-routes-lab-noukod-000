006ee4eef372d23639fa1335c8f688ddbab17324
Saint-Jean Germila <germila@noukod.com>
2019-04-29 15:17:28 +0000

    Automatically backed up by Learn

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,10 +4,10 @@ import { NavLink } from 'react-router-dom';
 const NavBar = () => {
   return (
     <div>
-    <NavLink to="/" exact style={link}>
-    <NavLink to="/Movies" exact style={link}>
-    <NavLink to="/Directors" exact style={link}>
-    <NavLink to="/Actors" exact style={link}>
+    <NavLink to="/" exact style={link}>Home</NavLink>
+    <NavLink to="/Movies" exact style={link}>Movies</NavLink>
+    <NavLink to="/Directors" exact style={link}>Directors</NavLink>
+    <NavLink to="/Actors" exact style={link}>Actors</NavLink>
     </div>;
   );
 };
